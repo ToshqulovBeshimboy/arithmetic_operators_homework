ba650d9e71fe02241175f57ccdf58845d58cccef
ToshqulovBeshimboy <tbeshimboy@gmail.com>
2025-12-03 12:14:55 +0500

    arithmetic_op05.py

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,7 +1,8 @@
 # n  deb nomlangan o'zgaruvchi yarating va unga to'rt xonali raqamni belgilang
-
+n=3589
 # n sonining teskarisini toping va uni total deb nomlangan o'zgaruvchiga belgilang.
-
+y=n%10*1000+n%100//10*100+n//100%10*10+n//1000
 # sub deb nomlanga o'zgaruvchi yarating va dastlabki n qiymatdan keyingi total qiymatni ayirmasini ta'minlang.
-
+sub=y
 # sub o'zgaruvchisini chiqaring
+print(sub)
