44176a5c537672751c32f1fc8e944332f3ac6c50
ToshqulovBeshimboy <tbeshimboy@gmail.com>
2025-12-03 12:10:00 +0500

    arithmetic_op04.py

--- arithmetic_op04.py
+++ arithmetic_op04.py
@@ -1,11 +1,12 @@
 # n deb nomlangan o'zgaruvchi yarating va unga uch xonali raqamni belgilang.
-
+n=234
 # n ning birinchi raqamini toping va x1 ga belgilang.
-
+x1=n//100
 # n ning ikkinchi raqamini toping va x2 ga belgilang.
-
+x2=n//10%10
 # n ning uchinchi raqamini toping va x3 ga belgilang.
-
+x3=n%10
 # total deb nomlangan o'zgaruvchi yarating va unga (x1 + x2 * x3) ushbu ifodani ta'minlan.
-
+y=x1+x2*x3
 # total qiymatini chop eting.
+print(y)
