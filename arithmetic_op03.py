df9324028a4f0031f4acf8aa186484701ea766a7
ToshqulovBeshimboy <tbeshimboy@gmail.com>
2025-12-03 12:04:52 +0500

    arithmetic_op03.py

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,6 @@
 # n  deb nomlangan o'zgaruvchi yarating va unga ikki xonali raqamni belgilang
-
+n=34
 # n sonining teskarisini toping va uni total deb nomlangan o'zgaruvchiga belgilang.
-
+x=n%10*10+n//10
 # total qiymatini chop eting
+print(x)
