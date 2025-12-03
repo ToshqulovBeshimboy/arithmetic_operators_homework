6e2017caebd3eb9e37e2c7240305ccb1049c40b8
ToshqulovBeshimboy <tbeshimboy@gmail.com>
2025-12-03 12:35:50 +0500

    arithmetic_op06.py

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,9 +1,10 @@
 # n  deb nomlangan o'zgaruvchi yarating va besh  xonali turli xil raqamni belgilang
-
+n=46318
 # n sonining raqamlar yig'indisni toping va uni sum deb nomlangan o'zgaruvchiga belgilang.
-
+sum=n%10+n%100//10+n//100%10+n//1000%10+n//10000
 # n sonining raqamlar ko'paymasini toping va uni k deb nomlangan o'zgaruvchiga belgilang.
-
+k=(n%10)*(n%100//10)*(n//100%10)*(n//1000%10)*(n//10000)
 # total deb nomlangan o'zgaruvchi yarating va k ning qiymatini sum qiymatiga butunli bo'ling. 
-
+m=k//sum
 # total o'zgaruvchisining natijasini chiqaring.
+print(m)
