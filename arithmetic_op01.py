88d5e57eed0b812285b44878f21a14121d4fbdce
ToshqulovBeshimboy <tbeshimboy@gmail.com>
2025-12-03 11:59:52 +0500

    arithmetic_op01.py

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -1,7 +1,11 @@
 # x  o'zgaruvchisini yarating va unga 10 qiymatini belgilang.
-
+x=10
 # y  o'zgaruvchisini yarating va unga 5 qiymatini belgilang.
-
+y=5
 # total o'zgaruvchisini yarating va unga (x * y) - a + b qiymatini belgilang.
+a=int(input())
+b=int(input())
+z=(x*y)-a+b
 
 # Natija qiymatini chop eting.
+print(z)
